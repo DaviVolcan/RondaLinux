f8517b434e3432e76191f8677419c00d5a80515e
davivolcan <davi.volcan2@gmail.com>
2025-08-17 21:08:05 -0300

    Switch StatusDisplay package to CMake build system and enable in bbg_volcan_defconfig

--- br-external/package/statusdisplay/statusdisplay.mk
+++ br-external/package/statusdisplay/statusdisplay.mk
@@ -5,17 +5,11 @@
 ################################################################################
 
 STATUSDISPLAY_VERSION = 1.0
-STATUSDISPLAY_SITE = $(BR2_EXTERNAL_RONDA_EXTERNAL_PATH)/package/statusdisplay/src
+STATUSDISPLAY_SITE = $(BR2_EXTERNAL_RONDA_EXTERNAL_PATH)/package/statusdisplay
 STATUSDISPLAY_SITE_METHOD = local
 STATUSDISPLAY_LICENSE = GPL-2.0+
 STATUSDISPLAY_LICENSE_FILES = LICENSE
 
-define STATUSDISPLAY_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
-endef
+STATUSDISPLAY_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
 
-define STATUSDISPLAY_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/statusdisplay $(TARGET_DIR)/usr/bin/statusdisplay
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
